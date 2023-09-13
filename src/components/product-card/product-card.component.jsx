959f59734bfdca6fb9505c187344ad64acefc88c
jbElmax <jan.elnas@gmail.com>
2023-09-13 10:08:15 +0800

    icon component  incremen t when added by items

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,15 @@
 import './product-card.styles.scss';
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 
 const ProductCard = ({product}) =>{
+    const {addItemToCart} = useContext(CartContext);
+
     const {id,name,imageUrl,price} = product;
+
+    const addProductToCart = ()=>addItemToCart(product);
+    
     return(
         <div className='product-card-container' key={id}>
             <img src={imageUrl} alt={name}/>
@@ -10,7 +17,7 @@ const ProductCard = ({product}) =>{
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to Cart</Button>
+            <Button buttonType='inverted'onClick={addProductToCart}>Add to Cart</Button>
         </div>
     )
 }
